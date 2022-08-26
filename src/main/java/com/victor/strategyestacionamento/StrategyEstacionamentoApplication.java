26463cd888256bae1f23fed23bd303d026c2239e
VictorKano27 <ctl_vkano@uolinc.com>
2022-08-26 09:58:38 -0300

    first commit

---
+++ src/main/java/com/victor/strategyestacionamento/StrategyEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.victor.strategyestacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyEstacionamentoApplication.class, args);
+	}
+
+}
